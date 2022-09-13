45098b8467708c61150b73d445bd13618e24518a
24190Eddy <112130852+24190Eddy@users.noreply.github.com>
2022-09-12 22:06:07 -0600

    Delete celcius2farenheit.py

--- celcius2farenheit.py
+++
@@ -1,6 +0,0 @@
-# Tendencias e Innovacion en Tecnologia Agricola
-
-temperaturaCelcius = float(input("Cuál es la temperatura en Celcius? "))
-temperaturaFarenheit = (9/5) * temperaturaCelcius +32
-
-print(temperaturaFarenheit)
